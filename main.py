0c107c8097325440c44a5b6d6c53ae5b84a9e39f
Mario Sanchez <mario.sanchez.m12@gmail.com>
2024-05-08 19:12:41 -0400

    Agregando archivos py del proyecto y primeros pasos del codigo

---
+++ main.py
@@ -0,0 +1,22 @@
+from funciones import *
+
+def Main():
+    while True:
+        opcion = int(input('Ingresa 1 para ver los pacientes de la clinica\nIngresa 2 para mostrar el detalle de un paciente\nIngresa 3 para cambiar a un paciente de cama\nIngresa 4 para cambiar a un paciente de medico\nIngresa 5 para crear camas y habitaciones\nIngresa 6 para salir: \n'))
+
+        if opcion == 1:
+            pass
+        elif opcion == 2:
+            pass
+        elif opcion == 3:
+            pass
+        elif opcion == 4:
+            pass
+        elif opcion == 5:
+            pass
+        elif opcion == 6:
+            break
+        else:
+            print('Opción Incorrecta')
+
+Main()
