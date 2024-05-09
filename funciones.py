6f1b60c334b0d2576ddd4a121a033f21f5499c42
Mario Sanchez <mario.sanchez.m12@gmail.com>
2024-05-09 17:09:08 -0400

    Avanzando en el desarrollo de algunas funciones

--- funciones.py
+++ funciones.py
@@ -15,4 +15,13 @@ def CambiarMedico():
     pass
 
 def CrearCamaYHabitacion():
-    pass
+    opcion = int(input('Ingresa 1 si deseas crear una cama, Ingresa 2 si deseas crear una habitación'))
+    if opcion == 1:
+        ncamas = int(input('Cuantas camas deseas crear: '))
+        CrearCamas(ncamas)
+    elif opcion == 2:
+        zona = input('Ingresa la zona donde deseas crear la habitacion: ')
+        ncamas2 = int(input('Ingresa el número de camas de la nueva habitación: '))
+        CrearHabitaciones(ncamas2,zona)
+    else:
+        print('Opción incorrecta')
--- models.py
+++ models.py
@@ -30,14 +30,35 @@ def FiltrarPacientes(search):
 
     conexion.close()
 
-def CambiarPacienteCama():
-    pass
+def CambiarPacienteCama(id_nueva_cama, id_paciente):
+    conexion = psycopg2.connect(host=config.host,database=config.database,user=config.user,password=config.password)
+    cursor = conexion.cursor() 
+    cursor.execute("UPDATE asignacioncama SET id_cama = %s WHERE id_paciente = %s", (id_nueva_cama, id_paciente))
+    conexion.commit()
+    print("Cambio de cama realizado con éxito.")
+    conexion.close()
 
-def CambiarMedicoPaciente():
-    pass
+def CambiarMedicoPaciente(id_nuevo_medico,id_paciente):
+    conexion = psycopg2.connect(host=config.host,database=config.database,user=config.user,password=config.password)
+    cursor = conexion.cursor() 
+    cursor.execute("UPDATE asignacionmedico SET id_medico = %s WHERE id_paciente = %s", (id_nuevo_medico, id_paciente))
+    conexion.commit()
+    print("Cambio de Medico realizado con éxito.")
+    conexion.close()
 
-def CrearCamas():
-    pass
+def CrearCamas(ncamas, disponible=True):
+    conexion = psycopg2.connect(host=config.host,database=config.database,user=config.user,password=config.password)
+    cursor = conexion.cursor() 
+    for _ in range(ncamas):
+        cursor.execute("INSERT INTO cama (disponible) VALUES (%s)", (disponible,))
+    conexion.commit()
+    print(f"{ncamas} camas creadas con éxito.")
+    conexion.close()
 
-def CrearHabitaciones():
-    pass
+def CrearHabitaciones(ncamas,zona ):
+    conexion = psycopg2.connect(host=config.host,database=config.database,user=config.user,password=config.password)
+    cursor = conexion.cursor() 
+    cursor.execute("INSERT INTO habitacion (zona, ncamas) VALUES (%s, %s)", (zona, ncamas))
+    conexion.commit()
+    print("Habitación creada con éxito.")
+    conexion.close()
